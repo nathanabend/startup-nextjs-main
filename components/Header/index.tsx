5e79ff9f310481d1d9ec498db696f3c6e5634583
unknown <nathan abend>
2023-07-11 11:55:25 +0100

    sixth commit

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -157,7 +157,7 @@ const Header = () => {
                   Sign In
                 </Link>*/}
                 <Link
-                  href="http://localhost:3000/form"
+                  href="http://blockheath.com/form"
                   className="ease-in-up hidden rounded-md bg-primary py-3 px-8 text-base font-bold text-white transition duration-300 hover:bg-opacity-90 hover:shadow-signUp md:block md:px-9 lg:px-6 xl:px-9"
                 >
                   Get Started {/*Sign Up*/}
